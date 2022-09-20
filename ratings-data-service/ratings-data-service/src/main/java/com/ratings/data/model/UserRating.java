5e303e88245e60cbf9c876d202d95cf1ea8f9c38
shoravs <sharmasaurav142@gmail.com>
2022-09-20 20:50:14 +0530

    modify the UserRating model class

---
+++ ratings-data-service/ratings-data-service/src/main/java/com/ratings/data/model/UserRating.java
@@ -0,0 +1,17 @@
+package com.ratings.data.model;
+
+import java.util.List;
+
+public class UserRating {
+
+	private List<Rating> listRatings;
+
+	public List<Rating> getListRatings() {
+		return listRatings;
+	}
+
+	public void setListRatings(List<Rating> listRatings) {
+		this.listRatings = listRatings;
+	}
+
+}
