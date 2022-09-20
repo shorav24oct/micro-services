3ef7a49679fbe4567ab686610a902fd755430a86
shoravs <sharmasaurav142@gmail.com>
2022-09-20 14:27:45 +0530

    create seprate thread pool for movie info bulkhead pattern

--- movie-catalog-service/src/main/java/com/movie/catalog/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/movie/catalog/MovieCatalogServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,6 +12,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SuppressWarnings("deprecation")
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
 	@Bean
