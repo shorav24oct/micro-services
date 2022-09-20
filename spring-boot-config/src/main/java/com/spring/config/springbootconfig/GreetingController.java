8cba570bd2b5a80a9b783ebdcdb3b14c45c62e2c
shoravs <sharmasaurav142@gmail.com>
2022-09-20 22:33:11 +0530

    added 3 different tricks for @Value and use @ConfigurationProperties

--- spring-boot-config/src/main/java/com/spring/config/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/spring/config/springbootconfig/GreetingController.java
@@ -1,18 +1,31 @@
 package com.spring.config.springbootconfig;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-	
-	@Value("${my.greetings}")
+
+	@Value("${my.greetings: default value}")
 	private String greetingMessage;
+
+	@Value("default static message ")
+	private String staticMessage;
+
+	@Value("${my.list.values}")
+	private List<String> listValues;
 	
+	@Autowired
+	DBConfig dbConfig;
+
 	@GetMapping("/greeting")
 	public String getGreetings() {
-		return greetingMessage;
+		return dbConfig.getConnection() + dbConfig.getHost() + dbConfig.getPort();
 	}
 
 }
